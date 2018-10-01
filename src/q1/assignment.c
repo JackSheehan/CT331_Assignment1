ffdd2f5d00b4f7cfb56f264cadf17e745a90cf51
JackSheehan <j.sheehan13@nuigalway.ie>
2018-10-01 14:50:53 +0100

    Question 1
    Question 1 completed as of 1/10/18

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+
+	int i = sizeof(i);
+	int* j = sizeof(j);
+	double* k = sizeof(k);
+	long l = sizeof(l);
+	char** c = sizeof(c);
+
+  printf("Int size %d \n", i);
+  printf("Int pointer size %d \n", j);
+  printf("double pointer size %d \n", k);
+  printf("long size %ld \n", l);
+  printf("char size %d \n", c);
+
   return 0;
 }
